06671680803cb8458c3af6cbc3022edfd955e935
Maksym Kurylo <Nalachik@ukr.net>
2022-03-10 20:09:49 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const TimeStorege_key = "videoplayer-current-time";
+
+const saveCurrentTime = ({ seconds }) => localStorage.setItem(TimeStorege_key, `${seconds}`);
+const throttledCurrentTime = throttle(saveCurrentTime, 1000);
+
+player.on('timeupdate', throttledCurrentTime);
+
+    const currentTime = localStorage.getItem(TimeStorege_key);
+
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+	
+     seconds = currentTime;
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
